57a2249902bfe37033abeb22bddce359663ac58d
Ubuntu <ubuntu@ip-172-31-38-179.eu-west-1.compute.internal>
2022-02-11 16:14:08 +0000

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #load environment variables
+source ../env_settings.sh
 meteor update --release 2.5.6 #ensure proper version of Meteor
 npm install # install NPM dependencies
 npm prune --production # remove development dependencies
 rm -rf ~/bundle # remove the previous bundle
-meteor build --directory ~ # build the current bundle 
-cd ~/bundle/programs/server # enter the bundle
+meteor build /home/ubuntu/build ~ # build the current bundle 
+cd /home/ubuntu/build/bundle/programs/server # enter the bundle
 npm install # install dependencies
-mv ~/bundle ~/portal 
+mv /home/ubuntu/build/bundle ~/portal 
 # make sure the logs directory exists
 mkdir ~/logs 
 # use forever to restart the Node.js server
